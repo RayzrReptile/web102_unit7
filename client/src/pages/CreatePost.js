69e05b8c23edd6b07d70edd717797d4dbe5a2ddd
Rayzr Reptile <padkins2020@fau.edu>
2024-04-09 17:58:35 -0400

    Create Posts

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { supabase } from '../client'
 import './CreatePost.css'
 
 const CreatePost = () => {
 
     const [post, setPost] = useState({title: "", author: "", description: ""})
 
-    const handleChange = (event) => {
+    const updateTestFields = (event) => {
         const {name, value} = event.target;
         setPost( (prev) => {
             return {
@@ -15,22 +16,33 @@ const CreatePost = () => {
         })
     }
 
+    const createPost = async (event) => {
+        event.preventDefault();
+      
+        await supabase
+          .from('Posts')
+          .insert({title: post.title, author: post.author, description: post.description})
+          .select();
+      
+        window.location = "/";
+    }
+
     return (
         <div>
             <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" onChange={handleChange} /><br />
+                <label for="title">Title</label><br />
+                <input type="text" id="title" name="title" onChange={updateTestFields} /><br />
                 <br/>
 
                 <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" onChange={handleChange} /><br />
+                <input type="text" id="author" name="author" onChange={updateTestFields} /><br />
                 <br/>
 
                 <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" onChange={handleChange}>
+                <textarea rows="5" cols="50" name="description" id="description" onChange={updateTestFields}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={createPost} />
             </form>
         </div>
     )
